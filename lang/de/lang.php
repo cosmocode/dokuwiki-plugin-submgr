bd8250320456e665ce91ca2db1ad32372c3c84cf
Andreas Gohr <gohr@cosmocode.de>
2016-06-21 16:33:37 +0200

    added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * German language file for submgr plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+
+$lang['menu'] = 'Automatisches Abo-Management';
+
+$lang['legend'] = 'Regel hinzufügen oder ersetzen';
+$lang['rules'] = 'Bestehende Regeln';
+
+$lang['item'] = 'Seite/Namensraum';
+$lang['type'] = 'Abo-Typ';
+$lang['members'] = 'Nutzer/Gruppen';
+
+$lang['remove'] = 'Entfernen';
+$lang['add'] = 'Hinzufügen';
+
+$lang['every'] = 'Jede Änderung';
+$lang['digest'] = 'Zusammenfassung';
+$lang['list'] = 'Liste von Seiten (nur Namenraum)';
+
+
+
+//Setup VIM: ex: et ts=4 :
